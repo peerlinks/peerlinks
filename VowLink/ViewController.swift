c1a45e32f63b409340f1ce98ee398c33caea151e
Fedor Indutny <fedor@indutny.com>
2019-07-29 08:38:32 +0200

    Initial Commit

---
+++ VowLink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VowLink
+//
+//  Created by Indutnyy, Fedor on 7/29/19.
+//  Copyright © 2019 Indutnyy, Fedor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
