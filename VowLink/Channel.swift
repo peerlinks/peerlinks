32da2c5898cdf1f12421b2a167d6d79f507db1cb
Fedor Indutny <fedor@indutny.com>
2019-07-29 17:31:01 +0200

    channel: fill

--- VowLink/Channel.swift
+++ VowLink/Channel.swift
@@ -7,7 +7,41 @@
 //
 
 import Foundation
+import Sodium
 
 class Channel {
+    let sodium = Sodium()
     
+    var publicKey: Bytes!
+    var secretKey: Bytes?
+    
+    private var lazyChannelID: Bytes?
+    var channelID: Bytes {
+        get {
+            if let id = lazyChannelID {
+                return id
+            }
+            lazyChannelID = sodium.genericHash.hash(message: publicKey,
+                                                    key: "vowlink-channel-id".bytes,
+                                                    outputLength: Channel.CHANNEL_ID_LENGTH)
+            return lazyChannelID!
+        }
+    }
+    
+    static let CHANNEL_ID_LENGTH = 32
+    
+    init() {
+        let keyPair = sodium.sign.keyPair()!
+        publicKey = keyPair.publicKey
+        secretKey = keyPair.secretKey
+    }
+    
+    init(publicKey: Bytes) {
+        self.publicKey = publicKey
+    }
+    
+    init(publicKey: Bytes, secretKey: Bytes) {
+        self.publicKey = publicKey
+        self.secretKey = secretKey
+    }
 }
