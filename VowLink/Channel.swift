2272cf580e3ab60249efc06c1878bbff175476cb
Fedor Indutny <fedor@indutny.com>
2019-07-29 16:54:05 +0200

    channel: initial

---
+++ VowLink/Channel.swift
@@ -0,0 +1,13 @@
+//
+//  Channel.swift
+//  VowLink
+//
+//  Created by Indutnyy, Fedor on 7/29/19.
+//  Copyright © 2019 Indutnyy, Fedor. All rights reserved.
+//
+
+import Foundation
+
+class Channel {
+    
+}
