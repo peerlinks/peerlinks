be0b5fb5eefb221c0ad0b96be9dd3db99f92fb6f
Fedor Indutny <fedor@indutny.com>
2019-08-09 18:51:55 -0700

    save

--- test/link-test.js
+++ test/link-test.js
@@ -15,17 +15,21 @@ describe('Link', () => {
   });
 
   it('should be issued by identity', () => {
-    const channelId = Buffer.alloc(Channel.ID_SIZE);
-    sodium.randombytes_buf(channelId);
+    const channel = new Channel('test-channel', issuer.publicKey);
 
     const trustee = new Identity('trustee');
 
     const link = issuer.issueLink({
-      channelId,
+      channel,
       trusteePubKey: trustee.publicKey,
     });
 
-    assert.ok(link.verify(channelId, issuer));
-    assert.ok(!link.verify(channelId, trustee));
+    assert.ok(link.verify(channel, issuer.publicKey));
+    assert.ok(!link.verify(channel, trustee.publicKey));
+
+    // Invalid because of timestamp
+    const ONE_YEAR = 365 * 24 * 3600 * 1000;
+    assert.ok(!link.verify(channel, issuer.publicKey,
+      Date.now() + ONE_YEAR));
   });
 });
